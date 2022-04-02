59f66af39407b0f6585ecf0265b3c5b16640878b
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2022-04-02 21:17:57 +0530

    add content

--- pages/gamescreen.js
+++ pages/gamescreen.js
@@ -2,16 +2,43 @@ import Image from 'next/image'
 import Footer from '../components/footer'
 import styles from '../styles/Game.module.css'
 
+
+function begin(){
+
+}
+
 export default function GameScreen(){
-return (
+  if(2>1){
+      return (
 <div>
     <main>
-<div className={styles.content}>
-<h1>hello</h1>
+<div className={styles.content} id="content" >
+<h1>people in the room: 3</h1>
+<button onClick={(e)=>begin()}>start</button>
+</div>
+
+
+    </main>
+
+    <Footer/>
+</div>
+      )
+
+  }
+  else{
+      return (
+<div>
+    <main>
+
+
+<div className={styles.content} id="gamecontent">
+<h1>game has started!!!</h1>
 </div>
     </main>
 
     <Footer/>
 </div>
-)
+      )
+  }
+
 }
